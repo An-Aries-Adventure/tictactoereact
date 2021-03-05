220fb6a5d65318a92ad47c0f9e6e4af4d53d71da
Kyle <kylespinaio@hotmail.com>
2021-03-05 17:48:27 -0500

    removed game class to allow divs to stack and not flex.

--- src/components/game.js
+++ src/components/game.js
@@ -48,16 +48,17 @@ class Game extends Component{
 
          return(
             <div className ="game">
+                <div className ="game-info">
+                    <div>{status}</div>
+                    <ol>{/*todo*/}</ol>
+                </div>
                 <div className='game-board'>
                     <Board
                     squares={current.squares}
                     onClick={(i) => this.handleClick(i)}
                     />
                 </div>
-                <div className ="game-info">
-                <div>{status}</div>
-                <ol>{/*todo*/}</ol>
-                </div>
+                
             </div>
         );
     };
